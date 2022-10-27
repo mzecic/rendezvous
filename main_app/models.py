b2f97b85ba648db3df0cdf096e978283c5aaf2b9
mzecic <matejzecic@gmail.com>
2022-10-27 18:01:45 -0400

    add model and detail

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f'{self.title} - {self.description}'
