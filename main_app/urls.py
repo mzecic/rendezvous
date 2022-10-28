d422e06a6e3c709ef7e7e01e6b548df02e066048
Keith Williams (willik01) <me@keithwilliams.net>
2022-10-28 12:03:58 -0700

    add authorization to edit/delete buttons for listing

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('listings/', views.listings_index, name='index'),
+    path('my_listings/', views.listings_index, name='my_listings'),
     path('listings/<int:listing_id>/', views.listings_detail, name='detail'),
     path('listings/create/', views.ListingCreate.as_view(), name='listings_create'),
     path('listings/<int:pk>/update/', views.ListingUpdate.as_view(), name='listings_update'),
